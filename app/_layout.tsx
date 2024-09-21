87f99e994f0e6f19aacd1bbca46c39717b725614
Willian Custodio Quintino <williancustodioquintino@gmail.com>
2024-09-21 22:01:12 +0000

    Feat: New page

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,41 @@
-import { Slot } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import { Drawer } from 'expo-router/drawer';
+import { Platform } from 'react-native';
+import { Link, Slot } from 'expo-router';
 
-export default function HomeLayout() {
+import { routesConfig } from './routesConfig';
+
+
+export default function Layout() {
+   if (Platform.OS !== 'web') {
+    // Use a basic custom layout on web.
+    return (
+      <div style={{ flex: 1 }}>
+        <header>
+          {Object.keys(routesConfig).map(key => (
+            <Link key={key} href={routesConfig[key].href}>{routesConfig[key].drawerLabel}</Link>
+          ))}
+        </header>
+        <Slot />
+      </div>
+    );
+  }
   return (
-    <>
-      <Slot />
-    </>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Drawer>
+        {Object.keys(routesConfig).map(key => {
+            return (
+              <Drawer.Screen
+                key={key}
+                name={key}
+                options={{
+                  drawerLabel: routesConfig[key].drawerLabel,
+                  title: routesConfig[key].title,
+                }}
+              />
+            );
+          })}
+      </Drawer>
+    </GestureHandlerRootView>
   );
 }
